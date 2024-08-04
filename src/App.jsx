e3945ff4bddb4ce4c3ec461c5ec781fbec880b4e
Fadem300 <fadel.m200@gmail.com>
2024-08-04 12:52:36 +0300

    c

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 // src/App.jsx
+import './App.css';
 
 const App = () => {
+  const items = [
+    { name: 'x', watched: true },
+    { name: 'y', watched: false },
+    { name: 'z', watched: true },
+  ];
 
   return (
-    <h1>Hello world!</h1>
+    <>
+      <h1>Shopping List</h1>
+      <ul>
+        {items.map((item, index) => (
+          <li key={index} >
+            {item.watched ? item.name+'✅✅✅✅✅' :item.name+'❌❌❌❌❌'}
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
-export default App
+export default App;
